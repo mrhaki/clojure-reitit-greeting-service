d01cab8e1952321e56932d56d51d126e1c843521
Hubert A. Klein Ikkink <h.kleinikkink@gmail.com>
2021-01-23 19:59:14 +0100

    Define middleware in route to convert request parameters

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
 (defproject clojure-reitit-greeting-service "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :license {:name "APACHE LICENSE, VERSION 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [http-kit "2.3.0"]
+                 [ring/ring-core "1.8.2"]
+                 [metosin/reitit "0.5.11"]]
   :main ^:skip-aot clojure-reitit-greeting-service.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/clojure_reitit_greeting_service/core.clj
+++ src/clojure_reitit_greeting_service/core.clj
@@ -1,7 +1,44 @@
 (ns clojure-reitit-greeting-service.core
+  (:require [org.httpkit.server :refer [run-server]]
+            [ring.util.response :refer [response]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+            [reitit.ring :as ring]
+            [reitit.ring.middleware.parameters :as parameters])
   (:gen-class))
 
+(def counter (atom 0))
+
+(defn greeting-handler [{{:keys [name] :or {name "World"}} :params}]
+  (let [message (format "Hello, %s!" name)]
+    (swap! counter inc)
+    (response {:id @counter :content message})))
+
+(def router
+  (ring/router
+    ["/greeting" {:get greeting-handler
+                  :name ::greeting}]
+    {:data {:middleware [parameters/parameters-middleware
+                         wrap-keyword-params]}}))
+
+(def app 
+  (ring/ring-handler 
+    router
+    (ring/create-default-handler)))
+
+(defn start-server []
+  (run-server app {:port 8080}))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Start server to serve /greeting endpoint"
   [& args]
-  (println "Hello, World!"))
+  (start-server))
+
+
+(comment
+  ;; Stop server
+  (server)
+
+  ;; Start server
+  (def server (start-server))
+
+  )
